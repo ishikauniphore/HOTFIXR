08fff92c5d4640c83cf9b28458cf8c1b51f82f87
Ubuntu <ubuntu@ip-10-40-4-124.us-west-2.compute.internal>
2026-08-14 21:25:27 +0000

    update script

--- run_upload_model.sh
+++ run_upload_model.sh
@@ -4,4 +4,4 @@ python3 merge_fsdp_model.py \
     --local_dir $LATEST_CKPT/actor \
     --target_dir /dev/shm/grpo_synthesis_models/$1/merged
 
-py upload_to_hf.py --model_name "/dev/shm/grpo_synthesis_models/$1/merged" --shorthand "generator_$1"
+py upload_to_hf.py --model_name "/dev/shm/grpo_synthesis_models/$1/merged" --shorthand "$1"
